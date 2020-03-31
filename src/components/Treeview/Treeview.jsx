332c28d208006f4def765cdd66cd15dd30436a18
rishav <rishav@nutanix.com>
2020-03-31 20:06:40 +0530

    Icons to add tree and node been added

--- src/components/Treeview/Treeview.jsx
+++ src/components/Treeview/Treeview.jsx
@@ -4,6 +4,11 @@ import TreeView from '@material-ui/lab/TreeView';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import TreeItem from '@material-ui/lab/TreeItem';
+import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
+import { Typography } from '@material-ui/core';
+
 
 const data = {
     id: 'root',
@@ -38,7 +43,18 @@ function RecursiveTreeView() {
     const classes = useStyles();
 
     const renderTree = (nodes) => (
-        <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+        <TreeItem style={{height: '24px'}} key={nodes.id} nodeId={nodes.id} label={
+            <div style={{display:'flex', justifyContent: 'space-between'}}>
+                <Typography align='center'>{nodes.name}</Typography>
+                {nodes.hasOwnProperty('children') && <div>
+                <IconButton >
+                    <LibraryAddIcon />
+                </IconButton>
+                <IconButton >
+                    <AddIcon />
+                </IconButton>
+            </div>}
+                </div>}>
             {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
         </TreeItem>
     );
